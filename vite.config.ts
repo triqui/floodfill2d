1f72c1cc58d3d28cd36f370ed4ea4cb524aa91e8
Emanuele Feronato <triqui@libero.it>
2026-03-06 16:41:42 +0100

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 export default defineConfig({
+    base: "/floodfill2d/",
     build: {
+        outDir: "dist",
         rollupOptions: {
             input: {
                 demo: resolve(__dirname, "demo/index.html")
